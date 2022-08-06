dcbd50a7be8194dca038b3bb9bb8a81f38652c09
Mostafa Ismail <mostafaismail@Mostafas-MacBook-Pro.local>
2022-08-06 23:29:15 +0200

    Implement 2 Products

---
+++ src/main/java/com/github/designpatterns/structural/factorymethod/product/ProductA.java
@@ -0,0 +1,9 @@
+package com.github.designpatterns.structural.factorymethod.product;
+
+public class ProductA implements Product {
+
+  @Override
+  public String doSomething() {
+    return "A";
+  }
+}
---
+++ src/main/java/com/github/designpatterns/structural/factorymethod/product/ProductB.java
@@ -0,0 +1,9 @@
+package com.github.designpatterns.structural.factorymethod.product;
+
+public class ProductB implements Product {
+
+  @Override
+  public String doSomething() {
+    return "B";
+  }
+}
