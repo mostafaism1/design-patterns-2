1d23c83fa898c6052e6e9270525410e1add6700e
Mostafa Ismail <mostafaismail@Mostafas-MacBook-Pro.local>
2022-08-06 23:31:00 +0200

    Create and implement the Client

---
+++ src/main/java/com/github/designpatterns/structural/factorymethod/Client.java
@@ -0,0 +1,29 @@
+package com.github.designpatterns.structural.factorymethod;
+
+import com.github.designpatterns.structural.factorymethod.creator.Creator;
+import com.github.designpatterns.structural.factorymethod.creator.CreatorA;
+import com.github.designpatterns.structural.factorymethod.creator.CreatorB;
+
+public class Client {
+
+  public static void main(String[] args) {
+    Creator creator;
+    Configuration config = readApplicationConfig();
+    if (Configuration.A.equals(config)) {
+      creator = new CreatorA();
+    } else {
+      creator = new CreatorB();
+    }
+    String result = creator.doSomethingWithProduct();
+    System.out.println(result);
+  }
+
+  private static Configuration readApplicationConfig() {
+    return Configuration.A;
+  }
+
+  private static enum Configuration {
+    A,
+    B,
+  }
+}
