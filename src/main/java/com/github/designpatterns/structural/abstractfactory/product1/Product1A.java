d429ae4de482ac62bc1fc6f71659c19914ba796e
Mostafa Ismail <mostafaismail@Mostafas-MacBook-Pro.local>
2022-08-08 13:33:26 +0200

    Implement Product1A and Product1B

---
+++ src/main/java/com/github/designpatterns/structural/abstractfactory/product1/Product1A.java
@@ -0,0 +1,9 @@
+package com.github.designpatterns.structural.abstractfactory.product1;
+
+public class Product1A implements Product1 {
+
+  @Override
+  public String doSomething() {
+    return "1A";
+  }
+}
---
+++ src/main/java/com/github/designpatterns/structural/abstractfactory/product1/Product1B.java
@@ -0,0 +1,9 @@
+package com.github.designpatterns.structural.abstractfactory.product1;
+
+public class Product1B implements Product1 {
+
+  @Override
+  public String doSomething() {
+    return "1B";
+  }
+}
